9fcb049562ead050ec5c4b6c4402d6942a8fca9a
Cheng <lichchchn@gmail.com>
2022-03-28 17:50:19 +0800

    nvim: enable ts matchup

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -34,7 +34,7 @@ M.config = function()
     }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     ignore_install = {},
     matchup = {
-      enable = false, -- mandatory, false will disable the whole extension
+      enable = true, -- mandatory, false will disable the whole extension
       -- disable = { "c", "ruby" },  -- optional, list of language that will be disabled
     },
     highlight = {
